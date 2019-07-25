98ab26b8972dadfb716672bf1d079040b6f76456
byLLIPyT <alex-tt@mail.ru>
2019-07-25 23:34:08 +0300

    Initial Commit

---
+++ HWork 2.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HWork 2.2
+//
+//  Created by Александр Уткин on 25/07/2019.
+//  Copyright © 2019 Александр Уткин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
